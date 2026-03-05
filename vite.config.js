4944efe34c409c41f9dc67aeaa2b06a7427f60d1
groverananya07 <groverananya189@gmail.com>
2026-03-05 13:43:34 +0530

    fix github pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/3d-portfolio/',   // IMPORTANT for GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
